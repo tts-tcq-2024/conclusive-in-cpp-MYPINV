910ab40536c8219271613aa6a71c8ff669e12532
Ankit Kumar <83908838+MYPINV@users.noreply.github.com>
2024-10-28 12:28:03 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,7 +1,7 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) { 
   if(value < lowerLimit) {
     return TOO_LOW;
   }

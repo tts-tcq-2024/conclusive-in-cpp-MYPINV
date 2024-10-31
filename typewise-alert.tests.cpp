a48a6f9ab395c7442f000e026f110d017b71cb98
Ankit Kumar <83908838+MYPINV@users.noreply.github.com>
2024-10-31 12:03:51 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,7 +1,23 @@
 #include <gtest/gtest.h>
 #include "typewise-alert.h"
 
-TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
-
-
+TEST(TypeWiseAlertTestSuite, InfersBreachWhenValueIsBelowLowerLimit) {
+    EXPECT_EQ(inferBreach(-5.0, 0, 35), TOO_LOW);
 }
+
+TEST(TypeWiseAlertTestSuite, InfersBreachWhenValueIsAboveUpperLimit) {
+    EXPECT_EQ(inferBreach(50.0, 0, 35), TOO_HIGH);
+}
+
+TEST(TypeWiseAlertTestSuite, InfersNormalWhenValueIsWithinLimits) {
+    EXPECT_EQ(inferBreach(20.0, 0, 35), NORMAL);
+}
+
+TEST(TypeWiseAlertTestSuite, InfersLowerBoundaryAsNormal) {
+    EXPECT_EQ(inferBreach(0.0, 0, 35), NORMAL);
+}
+
+TEST(TypeWiseAlertTestSuite, InfersUpperBoundaryAsNormal) {
+    EXPECT_EQ(inferBreach(35.0, 0, 35), NORMAL);
+}
+

1abc29d1e76a53da2692775a3cf13cb6f0a4f3b6
Ankit Kumar <83908838+MYPINV@users.noreply.github.com>
2024-10-31 12:06:11 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -21,3 +21,39 @@ TEST(TypeWiseAlertTestSuite, InfersUpperBoundaryAsNormal) {
     EXPECT_EQ(inferBreach(35.0, 0, 35), NORMAL);
 }
 
+TEST(TypeWiseAlertTestSuite, ClassifiesBreachForPassiveCoolingLowTemperature) {
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, -1.0), TOO_LOW);
+}
+
+TEST(TypeWiseAlertTestSuite, ClassifiesBreachForPassiveCoolingHighTemperature) {
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 36.0), TOO_HIGH);
+}
+
+TEST(TypeWiseAlertTestSuite, ClassifiesBreachForPassiveCoolingNormalTemperature) {
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 20.0), NORMAL);
+}
+
+TEST(TypeWiseAlertTestSuite, ClassifiesBreachForHiActiveCoolingLowTemperature) {
+    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1.0), TOO_LOW);
+}
+
+TEST(TypeWiseAlertTestSuite, ClassifiesBreachForHiActiveCoolingHighTemperature) {
+    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46.0), TOO_HIGH);
+}
+
+TEST(TypeWiseAlertTestSuite, ClassifiesBreachForHiActiveCoolingNormalTemperature) {
+    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 40.0), NORMAL);
+}
+
+TEST(TypeWiseAlertTestSuite, ClassifiesBreachForMedActiveCoolingLowTemperature) {
+    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1.0), TOO_LOW);
+}
+
+TEST(TypeWiseAlertTestSuite, ClassifiesBreachForMedActiveCoolingHighTemperature) {
+    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, 41.0), TOO_HIGH);
+}
+
+TEST(TypeWiseAlertTestSuite, ClassifiesBreachForMedActiveCoolingNormalTemperature) {
+    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, 39.0), NORMAL);
+}
+
